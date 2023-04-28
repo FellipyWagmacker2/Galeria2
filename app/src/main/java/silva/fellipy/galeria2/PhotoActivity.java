86f0ad597ddcb9ab7c45eca26e71bdc35d88c62b
Fellipy Wagmacker <fellipypereira@outlook.com>
2023-04-28 10:59:40 -0300

    Initial commit

---
+++ app/src/main/java/silva/fellipy/galeria2/PhotoActivity.java
@@ -0,0 +1,14 @@
+package silva.fellipy.galeria2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
