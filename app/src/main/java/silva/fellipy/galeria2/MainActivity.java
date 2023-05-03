f696b3201242f3d3159b00c4e0f64e194bd5caa4
Fellipy Wagmacker <fellipypereira@outlook.com>
2023-05-03 12:53:35 -0300

    Parte do código

--- app/src/main/java/silva/fellipy/galeria2/MainActivity.java
+++ app/src/main/java/silva/fellipy/galeria2/MainActivity.java
@@ -1,6 +1,7 @@
 package silva.fellipy.galeria2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain); // Obtendo elemento tbMain
+        setSupportActionBar(toolbar); // Indicando que tbMain deve ser considerado uma ActionBar
     }
 }
--- app/src/main/java/silva/fellipy/galeria2/PhotoActivity.java
+++ app/src/main/java/silva/fellipy/galeria2/PhotoActivity.java
@@ -1,6 +1,9 @@
 package silva.fellipy.galeria2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
 
@@ -10,5 +13,11 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto); // Obtendo elemento tbMain
+        setSupportActionBar(toolbar); // Indicando que tbMain deve ser considerado uma ActionBar
+
+        ActionBar actionBar = getSupportActionBar(); // Obtendo a ActionBar
+        actionBar.setDisplayHomeAsUpEnabled(true); // Habilitando o botão de voltar
     }
 }
